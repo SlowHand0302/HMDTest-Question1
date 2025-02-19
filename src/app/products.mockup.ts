70cb54ea2f2a2fadf322f9a14156c0ff860d22ce
Nguyen Nhat Thong <work.nguyennhatthong@gmail.com>
2025-02-19 17:33:52 +0700

    initial commit

---
+++ src/app/products.mockup.ts
@@ -0,0 +1,47 @@
+export interface Product{
+    name: string,
+    price: number,
+}
+
+export const PRODUCT_LIST: Product[] = [
+    {
+        name: 'Product 1',
+        price: 100,
+    },
+    {
+        name: 'Product 2',
+        price: 200,
+    },
+    {
+        name: 'Product 3',
+        price: 300,
+    },
+    {
+        name: 'Product 4',
+        price: 400,
+    },
+    {
+        name: 'Product 5',
+        price: 500,
+    },
+    {
+        name: 'Product 6',
+        price: 600,
+    },
+    {
+        name: 'Product 7',
+        price: 700,
+    },
+    {
+        name: 'Product 8',
+        price: 800,
+    },
+    {
+        name: 'Product 9',
+        price: 900,
+    },
+    {
+        name: 'Product 10',
+        price: 1000,
+    }
+]
